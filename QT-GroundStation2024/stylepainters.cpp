8d9b15950f6ce3e614adc7371d822adba5bbe24e
Weaver Goldman <wlgoldman@wpi.edu>
2024-01-15 18:06:16 -0500

    Add back the current point.

--- QT-GroundStation2024/stylepainters.cpp
+++ QT-GroundStation2024/stylepainters.cpp
@@ -834,7 +834,7 @@ void HPRCStyle::drawHPRCPayloadMap(QPainter *p, const hprcDisplayWidget *w)
 
         QPoint center = rescaledImageBox.center();
 
-        // p->drawEllipse(center, pointRadius, pointRadius);
+        p->drawEllipse(center, pointRadius, pointRadius);
 
         // Draw the target payload position
         p->setBrush(QBrush(QColor(180, 20, 20, 150)));
