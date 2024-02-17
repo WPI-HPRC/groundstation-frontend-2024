e1d171532642b3f1d08a03ceea41b5f4afacb7e4
Will Scheirey <williamscheirey@gmail.com>
2024-02-17 15:33:40 -0500

    debug websockets macro

--- QT-GroundStation2024/Util/hprcwebsocket.cpp
+++ QT-GroundStation2024/Util/hprcwebsocket.cpp
@@ -1,6 +1,8 @@
 #include "hprcwebsocket.h"
 #include <iostream>
 
+#define DEBUG_WEBSOCKETS 0
+
 HPRCWebSocket::HPRCWebSocket(QString hostname, int port): hostname(hostname), port(port)
 {
     m_socket = new QWebSocket();
@@ -10,6 +12,7 @@ HPRCWebSocket::HPRCWebSocket(QString hostname, int port): hostname(hostname), po
     connect(m_socket, SIGNAL(connected()), this, SLOT(onConnected()));
     connect(m_socket, SIGNAL(disconnected()), this, SLOT(onDisconnected()));
 
+#if DEBUG_WEBSOCKETS
     connect(m_socket, SIGNAL(sslErrors(QList<QSslError>)), this, SLOT(sslErrors(QList<QSslError>)));
 
     connect(m_socket, SIGNAL(stateChanged(QAbstractSocket::SocketState)), this, SLOT(stateChanged(QAbstractSocket::SocketState)));
@@ -20,6 +23,7 @@ HPRCWebSocket::HPRCWebSocket(QString hostname, int port): hostname(hostname), po
     connect(m_socket, SIGNAL(aboutToClose()), this, SLOT(aboutToClose()));
     connect(m_socket, SIGNAL(bytesWritten(qint64)), this, SLOT(bytesWritten(qint64)));
     connect(m_socket, SIGNAL(readChannelFinished()), this, SLOT(readChannelFinished()));
+#endif
 
     connect(m_socket, SIGNAL(textMessageReceived(QString)), this, SLOT(messageReceived(QString)));
     connect(this, SIGNAL(open(QUrl)), m_socket, SLOT(open(QUrl)));
@@ -36,7 +40,9 @@ void HPRCWebSocket::connectToServer()
 
 void HPRCWebSocket::_connectToServer()
 {
+#if DEBUG_WEBSOCKETS
     qDebug("Attemtping to connect to server");
+#endif
     emit open(QUrl("wss://hprc-test.entflammen.com:8000"));
 }
 
@@ -58,16 +64,22 @@ void HPRCWebSocket::messageReceived(QString message)
 
 void HPRCWebSocket::onConnected()
 {
+#if DEBUG_WEBSOCKETS
     qDebug("Websocket Connected");
+#endif
 }
 
 void HPRCWebSocket::onDisconnected()
 {
+#if DEBUG_WEBSOCKETS
     qDebug("Websocket Disconnected");
+#endif
 
     if(retryConnection)
     {
+#if DEBUG_WEBSOCKETS
         qDebug("Trying to reconnect");
+#endif
         QTimer::singleShot(3000, this, &HPRCWebSocket::_connectToServer);
     }
 }
